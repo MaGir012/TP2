1599bdbb4a38dbf9514b9ebc3e0e79afa9aa3a8a
Maxime Girard <maximegirard.012@gmail.com>
2023-06-22 13:31:39 -0400

    Ajout du fetch pour communiquer avec l'API

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // API à utiliser pour le travail: https://randomuser.me/api/?results=5
+fetch('https://randomuser.me/api/?results=5')
+    .then(response => response.json())
+    .then(data => {
+        const cards = document.getElementById('cards');
+        data.results.forEach(result => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+                        <img src="${result.picture.large}" alt="Photo de profil">
+                        <h2>Nom: ${result.name.title} ${result.name.first} ${result.name.last}</h2>
+                        <p>Email: ${result.email}</p>
+                    `;
+            cards.appendChild(card);
+        });
+    })
+    .catch(error => console.error(error));
